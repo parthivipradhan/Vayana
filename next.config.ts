c4ee3f5eae280c7b8e24c1b4d323b8f406a86953
parthivipradhan <parthivipradhan@gmail.com>
2025-05-23 08:32:28 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ['images.unsplash.com', 'i.postimg.cc'], // List your allowed domains here
+  },
+};
+
+export default nextConfig;
