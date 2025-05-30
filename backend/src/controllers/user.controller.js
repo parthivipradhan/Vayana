493641c1b0f29a299eaade856c6d2ab9d7e868f2
Dev Prashant <dev.prashant1901@gmail.com>
2025-05-30 14:32:54 +0530

    .

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -222,52 +222,6 @@ const getUserProfile = asyncHandler(async(req, res) => {
                 username: username?.toLowerCase()
             }
         },
-        {
-            $lookup: {
-                from: "subscriptions",
-                localField: "_id",
-                foreignField: "channel",
-                as: "subscribers"
-            }
-        },
-        {
-            $lookup: {
-                from: "subscriptions",
-                localField: "_id",
-                foreignField: "subscriber",
-                as: "subscribedTo"
-            }
-        },
-        {
-            $addFields: {
-                subscribersCount: {
-                    $size: "$subscribers"
-                },
-                channelsSubscribedToCount: {
-                    $size: "$subscribedTo"
-                },
-                isSubscribed: {
-                    $cond: {
-                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
-                        then: true,
-                        else: false
-                    }
-                }
-            }
-        },
-        {
-            $project: {
-                fullName: 1,
-                username: 1,
-                subscribersCount: 1,
-                channelsSubscribedToCount: 1,
-                isSubscribed: 1,
-                avatar: 1,
-                coverImage: 1,
-                email: 1
-
-            }
-        }
     ])
 
     if (!channel?.length) {
@@ -277,7 +231,7 @@ const getUserProfile = asyncHandler(async(req, res) => {
     return res
     .status(200)
     .json(
-        new ApiResponse(200, channel[0], "User channel fetched successfully")
+        new ApiResponse(200, channel[0], "User fetched successfully")
     )
 })
 
